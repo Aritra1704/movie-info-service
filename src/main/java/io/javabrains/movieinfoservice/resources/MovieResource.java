5b4064d62251b490e55a6998172b34a7c226c509
Aritra1704 <aritra1704@gmail.com>
2021-05-17 19:59:00 +0530

    Initial commit

---
+++ src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,21 @@
+package io.javabrains.movieinfoservice.resources;
+
+import io.javabrains.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public List<Movie> getMovie(@PathVariable("movieId") String movieId) {
+        return Collections.singletonList(
+                new Movie(movieId, "Test name")
+        );
+    }
+}
